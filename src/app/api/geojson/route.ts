f03fb7d00b67b080af80d23423726d8fdde0100c
kimbeehexa <hoang.kim@beehexa.com>
2025-03-12 19:35:24 +0700

    fix: loading geojson

--- src/app/api/geojson/route.ts
+++ src/app/api/geojson/route.ts
@@ -1,8 +1,6 @@
 export const runtime = 'edge';
 
 import { NextResponse } from "next/server";
-import path from "path";
-import fs from "fs/promises";
 
 export async function GET(req: Request) {
     const { searchParams } = new URL(req.url);
@@ -12,14 +10,6 @@ export async function GET(req: Request) {
         return NextResponse.json({ error: "Layer parameter is required" }, { status: 400 });
     }
 
-    try {
-        const geojsonPath = process.env.GEOJSON_PATH || "./public/geojson";
-        const filePath = path.join(process.cwd(), geojsonPath, `${layer}.json`);
-
-        const geojsonData = await fs.readFile(filePath, "utf-8");
-        return NextResponse.json(JSON.parse(geojsonData), { status: 200 });
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: "GeoJSON file not found" }, { status: 404 });
-    }
+    // Serve the file directly from the public folder
+    return NextResponse.redirect(`/geojson/${layer}.json`);
 }
